0fdabf37954a2af018d00e2f6261a0380e056650
Waterworks <waterplant@petalmail.com>
2022-10-20 15:25:26 +0800

    update

---
+++ todb.py
@@ -0,0 +1,9 @@
+# A file to store the excel tickr file as a database
+
+import pandas as pd
+import sqlite3 as sq
+
+tkr = pd.read_excel('Yahoo Ticker Symbols.xlsx')
+db = sq.connect('stocks.db')
+tkr.to_sql("tkrinfo", db)
+db.close()
